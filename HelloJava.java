e45799143afc2b2760118cae3fd4274a6781e94b
DipeshMaywade <69308870+DipeshMaywade@users.noreply.github.com>
2021-02-09 10:54:08 +0530

    [Deep]Update: update from remote

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,6 @@
 public class HelloJava {
         public static void main(String[] args) {
                 System.out.println("Hello");
+                Syetem.out.println("Hello From Remote ")
         }
 }
